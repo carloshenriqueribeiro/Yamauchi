db651e1875a15f9fe18b4bec3e32ca747c8cdac2
Carlos Henrique Ribeiro <carloshoribeiro@gmail.com>
2017-02-21 02:05:38 -0300

    Commit
    solution asp.net MVC

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YamauchiEstoque.DAO;
+using YamauchiEstoque.Filtros;
+using YamauchiEstoque.Models;
+namespace YamauchiEstoque.Controllers
+{
+    [AutorizacaoFilter]
+    public class CategoriaController : Controller
+    {
+        //
+        // GET: /Produto/
+
+        public ActionResult Index()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(CategoriaDoProduto categoria)
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            dao.Adiciona(categoria);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/ContadorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YamauchiEstoque.DAO;
+using YamauchiEstoque.Models;
+namespace YamauchiEstoque.Controllers
+{
+    public class ContadorController : Controller
+    {
+        public ActionResult Index()
+        {
+            int contador = Convert.ToInt32(Session["contador"]);
+            contador++;
+            Session["contador"] = contador;
+            return View(contador);
+        }
+    }
+}
